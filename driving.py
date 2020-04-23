611ad86dd3876e663e0398a5ac75e56b223aff09
Ken Chung <phbchung@gmail.com>
2020-04-23 13:00:39 +0800

    add els part

--- driving.py
+++ driving.py
@@ -7,8 +7,11 @@ if country == '台灣':
 	else:
 		print('你還不能考駕照')
 
-if country == '美國':
+elif country == '美國':
 	if age >= 16:
 		print('你可以考駕照')
 	else:
-		print('你還不能考駕照')
+		print('你還不能考駕照') 
+else:
+	print('你只能輸入台灣/美國')
+	
